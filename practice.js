bc6df1a9cf6c0da3561a97357bd9d3c1a4a6f164
ashoordoushow <ashoor12323@gmail.com>
2024-10-08 12:50:46 -0500

    basic javascript deliberate practice

---
+++ practice.js
@@ -0,0 +1,40 @@
+// console.log('hello');
+
+// 1 // Write a program that uses a variable to store a number, then prints that number plus 10.
+
+// var number = 2; //this is how you write a variable in javascript
+// console.log(number + 10); //this is how you write the value of the variable plus an integer, 'console.log()' prints results in javascripts console
+
+// var x = 300;
+// console.log(x * 2);
+
+// 2 // Write a program that uses a variable to store a word, then prints that word with all capital letters.
+
+var word = 'hello';
+console.log(word.toUpperCase()); // this is the capital letters method used in javascript
+
+var definition = 'chronological';
+console.log(definition.toUpperCase());
+
+// 3 // Write a program that uses variables to store two numbers, then prints the numbers added together.
+
+var x = 30;
+var y = 23;
+console.log(x + y);
+
+// 4 // Write a program that uses a variable to store a word, then prints that word in reverse order.
+
+console.log(word.split("").reverse().join(""));
+console.log(word.split("").reverse().join(""));
+console.log(word.split("").reverse().join(""));
+console.log(word.split("").reverse().join(""));
+
+var word = "hello";
+console.log(word.split("").reverse().join("")); // this is the 3 javascript methods you must add to print your word in reverse order
+
+// 5 // Write a program that uses a variable to store a number, then prints the number times 10.
+
+var number = 2;
+console.log(number * 10);
+
+
