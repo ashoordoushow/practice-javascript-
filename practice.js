b9c9a7b96c8bef7977e52533250513e76cb03f6c
ashoordoushow <ashoor12323@gmail.com>
2024-10-09 11:36:53 -0500

    completed link1 Basic deliberate practice

--- practice.js
+++ practice.js
@@ -1,4 +1,4 @@
-// console.log('hello');
+// BASIC DELIBERATE PRACTICE LINK1
 
 // 1 // Write a program that uses a variable to store a number, then prints that number plus 10.
 
@@ -36,5 +36,64 @@ console.log(word.split("").reverse().join("")); // this is the 3 javascript meth
 
 var number = 2;
 console.log(number * 10);
+
+var x = 30;
+console.log(x * 30);
+
+var number = 2;
+console.log(number * 10);
+
+// 6 // Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
+
+var word1 = "proper";
+var word2 = "very nice";
+console.log(word1.toUpperCase() + " "  + word2.toUpperCase());
+
+var definition1 = "google";
+var definition2 = "is a very useful resource";
+console.log(definition1.toUpperCase() + " " + definition2.toUpperCase());
+
+// 7 // Write a program that uses a variable to store a word, then prints the number of letters in the word.
+
+var word = "city";
+console.log(word.length);
+
+var word2 = "techincallity";
+console.log(word2.length);
+
+// 8 // Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
+
+var number = 3.6356;
+console.log(parseInt(number));
+
+var number3 = 4.9;
+console.log(parseInt(number3)); // 'parseInt()' converts decimals to integers but always round down
+
+// 9 // Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
+
+var number1 = 23;
+var number2 = 45;
+console.log(number1 * number2);
+
+// 10 // Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
+
+var word = "BECAUSE";
+console.log(word.toLowerCase());
+
+var word1 = "BASKETBALL";
+console.log(word1.toLowerCase());
+
+var word2 = "MAGICIAN";
+console.log(word2.toLowerCase());
+
+var word3 = "JAVASCRIPT";
+console.log(word3.toLocaleLowerCase());  // this is also another way of writing this function to print everything in lower case letters
+
+// BASIC DELIBERATE PRACTICE LINK2
+
+
+
+
+
 
 
