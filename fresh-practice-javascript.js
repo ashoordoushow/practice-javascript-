ec5c684478f628123e9197d3538b0cbd4ed56783
ashoordoushow <ashoordoushow23@gmail.com>
2026-04-13 16:48:29 -0500

    chore: JavaScript Conditional Statements

--- fresh-practice-javascript.js
+++ fresh-practice-javascript.js
@@ -33,17 +33,17 @@
 
 // 5 Write a program that uses a variable to store a number, then prints the number times 10.
 
-let number = 23;
+// let number = 23;
 
-console.log(number * 10);
+// console.log(number * 10);
 
 
 // 6 Write a program that uses variables to store two words, then prints both words on the same line in all capital letters.
 
-let word1 = "Hello";
-let word2 = "Goodbye";
+// let word1 = "Hello";
+// let word2 = "Goodbye";
 
-console.log(word1.toUpperCase() + word2.toUpperCase());
+// console.log(word1.toUpperCase() + word2.toUpperCase());
 
 
 // 7 Write a program that uses a variable to store a word, then prints the number of letters in the word.
@@ -54,24 +54,71 @@ console.log(word1.toUpperCase() + word2.toUpperCase());
 
 // 8 Write a program that uses a variable to store a number with decimals, then prints the number as an integer.
 
-let decimalNumber = 2.6212;
+// let decimalNumber = 2.6212;
 
-console.log(Math.round(decimalNumber));
+// console.log(Math.round(decimalNumber));
 
 
 // 9 Write a program that uses a variable to store two numbers, then prints the two numbers multiplied together.
 
-let number1 = 233;
-let number2 = 9000;
+// let number1 = 233;
+// let number2 = 9000;
 
-console.log(number1 * number2);
+// console.log(number1 * number2);
 
 
 // 10 Write a program that uses a variable to store a word, then prints the word with all lowercase letters.
 
-let word = "basketBALL";
+// let word = "basketBALL";
 
-console.log(word.toLocaleLowerCase());
+// console.log(word.toLocaleLowerCase());
+
+// Deliberate Practice Link2 - Write if statements with valid syntax
+
+// 1 Use a variable to store a number, then write a condition that prints 0 if the number is equal to 10, and prints -1 otherwise.
+
+// let number = 2222;
+
+// if (number === 10) {
+//   console.log(0);
+// } else {
+//   console.log(-1);
+// }
+
+// 2  Use a variable to store a number, then write a condition that prints -1 if the number is less than 10, prints 1 if the number is greater than 10, and prints 0 if the number is equal to 10.
+
+let number = 433;
+
+if (number < 10) {
+  console.log(-1);
+} else if (number > 10) {
+  console.log(1);
+} else {
+  console.log(0);
+}
+
+// 3 Use variables to store two numbers, then write a condition that prints 1 if the numbers are both less than 10, and prints 0 otherwise.
+
+
+// 4 Use a variable to store a number, then write a condition that prints 1 if the number is over 9000, and prints -1 otherwise.
+
+
+// 5 Use a variable to store a number, then write a condition that prints 9 if the number is less than 10, prints 19 if the number is less than 20, prints 29 if the number is less than 30, and prints -1 otherwise (only one print statement should occur).
+
+
+// 6 Use variables to store two numbers, then write a condition that prints 100 if either number is greater than 10, and prints -100 otherwise.
+
+
+// 7 Use a variable to store a number, then write a condition that prints 1776 if the number is less than 0, and prints 1979 otherwise.
+
+
+// 8 Use a variable to store a number, then write a condition that prints 100 if the number equals 100, prints 99 if the number is equal to 99, and prints 0 otherwise.
+
+
+// 9 Use variables to store two numbers, then write a condition that prints 1 if the first number is less than zero and the second number is greater than 0, and prints 0 otherwise.
+
+
+// 10 Use a variable to store a number, then write a condition that prints 5 if the number is greater than 80, prints 4 if the number is greater than 60, prints 3 if the number is greater than 40, prints 2 if the number is greater than 20, and prints 1 otherwise (only one print statement should occur).
 
 
 
