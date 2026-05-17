4ffb829da1b6ef6efe482aac3bcd3eb1d967865e
ashoordoushow <ashoordoushow23@gmail.com>
2026-05-17 01:49:03 -0500

    JavaScript Custom Class(s) with Custom Methods

--- fresh-practice-javascript.js
+++ fresh-practice-javascript.js
@@ -1274,16 +1274,202 @@
 // Start with an array of numbers and compute the maximum number.
 // For example, [5, 10, 8, 3] becomes 10.
 
-const numbers = [7, 4, 23, 11];
-let maxNumber = 0;
+// const numbers = [7, 4, 23, 11];
+// let maxNumber = 0;
 
-numbers.forEach(function (number) {
-  if (number > maxNumber) {
-    return maxNumber = number;
+// numbers.forEach(function (number) {
+//   if (number > maxNumber) {
+//     return maxNumber = number;
+//   }
+// });
+// console.log(maxNumber);
+
+
+//  * computation — the process of a computer performing calculations, logic, transformations, or operations on data to produce a result/output
+
+
+// Deliberate Practice INTERMEDIATE level link 5 - Write a custom class with custom methods
+
+// Write a Song class with attributes and reader/writer methods for name, artist, and duration. Then write a method that prints the name, artist, and duration in a single sentence.
+
+// class Song {
+//   constructor(name, artist, duration) {
+//     this.name = name;
+//     this.artist = artist;
+//     this.duration = duration;
+//   }
+
+//   singleSentence() {
+//     return `The song is ${this.name}, by ${this.artist}, is ${this.duration} minutes long.`;
+//   }
+// }
+
+// let song = new Song("Ice", "Drake", 10);
+// console.log(song.singleSentence());
+
+
+
+// Write a Rectangle class with attributes and reader/writer methods for width and height. Then write a method that returns the area of the rectangle.
+
+// class Rectangle {
+//   constructor(width, height) {
+//     this.width = width;
+//     this.height = height;
+//   }
+
+//   area() {
+//     return this.width * this.height;
+//   }
+// }
+
+// let rectangle = new Rectangle(12, 12);
+// console.log(rectangle.area());
+
+// Write a Person class with attributes and reader/writer methods for name and age. Then write a method that returns the person's name in all capital letters.
+
+class Person {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
   }
-});
-console.log(maxNumber);
+  
+  toCapitalLetters() {
+    return this.name.toUpperCase();
+  }
+}
+
+let person = new Person("bob", 23);
+console.log(person.toCapitalLetters());
+
+// Write a Coordinate class with attributes and reader/writer methods for latitude and longitude. Then write a method that prints out the latitude and longitude in a single sentence.
+
+class Coordinate {
+  constructor(latitude, longitude) {
+    this.latitude = latitude;
+    this.longitude = longitude;
+  }
+
+  singleSentence() {
+    return `${this.latitude}, ${this.longitude}`;
+  }
+}
+
+let coordinates = new Coordinate(21.3, 123);
+console.log(coordinates.singleSentence());
+
+// Write an Account class with attributes and reader/writer methods for name and balance. Then write a method that prints a warning if the balance is below $100.
+
+class Account {
+  constructor(name, balance) {
+    this.name = name;
+    this.balance = balance;
+  }
+
+  belowHundred() {
+    if (this.balance < 100) {
+      return "Warning balance is too low!";
+    }
+  }
+}
+
+let account = new Account("bob", 12);
+console.log(account.belowHundred());
 
 
+// Write a Movie class with attributes and reader/writer methods for title, director, and year. Then write a method that prints out the attributes in a single sentence.
+
+class Movie {
+  constructor(title, director, year) {
+    this.title = title;
+    this.director = director;
+    this.year = year;
+  }
+
+  singleSentence() {
+    return `Movie is = ${this.title} ${this.director} ${this.year}`;
+  }
+}
+
+let movie = new Movie("Boom", "bob", 2012);
+console.log(movie.singleSentence());
 
 
+// Write a Car class with attributes and reader/writer methods for make, model, year, and color. Then write a method that returns the make and model as a single sentence in all lowercase letters.
+
+class Car {
+  constructor(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+  }
+
+  singleSentence() {
+    return `${this.make}${this.model}${this.year}`;
+  }
+}
+
+let car = new Car("honda", "accord", 2032);
+console.log(car.singleSentence());
+
+
+// Write a Point class with attributes and reader/writer methods for x, y, and z coordinates. Then write a method that returns true if all 3 numbers are positive, otherwise it returns false.
+
+class Point {
+  constructor(x, y, z) {
+    this.x = x;
+    this.y = y;
+    this.z = z;
+  }
+
+  truthyOrFalsy() {
+    if (this.x % 2 === 0 && this.y % 2 === 0 && this.z % 2 === 0) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+let point = new Point(2, 86, 12);
+console.log(point.truthyOrFalsy());
+
+
+// Write a Book class with attributes and reader/writer methods for title, author, and year. Then write a method that returns "Classic" if the book is older than 2000, otherwise it returns "Modern".
+
+class Book {
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  }
+
+  classicOrModern() {
+    if (this.year < 2000 ) {
+      return "Classic";
+    } else {
+      return "Modern";
+    }
+  }
+}
+
+let book = new Book("rich", "mike", 1999);
+console.log(book.classicOrModern());
+
+
+// Write a Plant class with attributes and reader/writer methods for name, size, and price. Then write a method that prints out the attributes in a single sentence.
+
+
+class Plant {
+  constructor(name, size, price) {
+    this.name = name;
+    this.size = size;
+    this.price = price;
+  }
+
+  singleSentence() {
+    return `${this.name}${this.size}${this.price}`;
+  }
+}
+
+let plant = new Plant("sunflower", "small", 900);
+console.log(plant.singleSentence());
