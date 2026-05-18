aaa71979df2e2ea68a1e0a3dc99b918f8bba5280
ashoordoushow <ashoordoushow23@gmail.com>
2026-05-17 19:33:40 -0500

    Converting data types JavaScript

--- fresh-practice-javascript.js
+++ fresh-practice-javascript.js
@@ -1626,20 +1626,20 @@
 // Use a nested loop with one array of strings to create a new array that contains every combination of each string with every other string in the array.
 // For example, ["a", "b", "c", "d"] becomes ["ab", "ac", "ad", "ba", "bc", "bd", "ca", "cb", "cd", "da", "db", "dc"].
 
-const strings = ["a", "b", "c"]; 
-const stringsCombined = [];
-let index = 0;
-while (index < strings.length) {
-  let index1 = 0;
-  while (index1 < strings.length) {               
-    if (index !== index1) {
-      stringsCombined.push(strings[index] + strings[index1]);                        
-    }               
-    index1 += 1;                                    //else statement is not needed here so it always runs.
-  }
-  index += 1;                
-}
-console.log(stringsCombined);
+// const strings = ["a", "b", "c"]; 
+// const stringsCombined = [];
+// let index = 0;
+// while (index < strings.length) {
+//   let index1 = 0;
+//   while (index1 < strings.length) {               
+//     if (index !== index1) {
+//       stringsCombined.push(strings[index] + strings[index1]);                        
+//     }               
+//     index1 += 1;                                    //else statement is not needed here so it always runs.
+//   }
+//   index += 1;                
+// }
+// console.log(stringsCombined);
 
 
 
@@ -1671,4 +1671,66 @@ console.log(stringsCombined);
 
 // Use a nested loop to convert an array of string arrays into a single string.
 // For example, [["a", "man"], ["a", "plan"], ["a", "canal"], ["panama"]] becomes "amanaplanacanalpanama".
+
+
+// Deliberate Practice ADVANCED LEVEL - link 3 - Convert data from one data type into another
+
+// Convert an array of arrays into a hash.
+// For example, [[1, 3], [8, 9], [2, 16]] becomes {1 => 3, 8 => 9, 2 => 16}. (This is RUBY example syntax)
+
+const numberPairs = [[2, 1], [21, 3], [98, 4]];
+let hashNumbers = {};
+
+numberPairs.forEach(function (numbers) {
+  return hashNumbers[numbers[0]] = numbers[1];          // .numbers is assignment/creation / [numbers[0]] is access/READ operation
+});
+console.log(hashNumbers);
+/* 
+  i need to take the the first valus of the array which is a pair of numbers in array itself
+  and put it in a hash making the first number the key, and the second value the value ex. 1: 3 , ex. name: "bob"
+  how
+  how do you push a value to a hash in javascript 
+  numberPairs.forEach(function (numbers) {
+    return hashNumbers = numbers[0] = numbers[1];
+  });
+
+
+*/
+
+
+
+// Convert an array of hashes into a hash using the :id key from the array's hashes as the keys in the new hash.
+// For example, [{id: 1, color: "blue", price: 32}, {id: 2, color: "red", price: 12}] becomes {1 => {id: 1, color: "blue", price: 32}, 2 => {id: 2, color: "red", price: 12}}.
+
+
+// Convert a string into a hash with keys for each letter in the string and values for the number of times the letter appears in the string.
+// For example, "bookkeeper" becomes {"b" => 1, "o" => 2, "k" => 2, "e" => 3, "p" => 1, "r" => 1}.
+
+
+// Convert a hash into an array of arrays.
+// For example, {"chair" => 100, "book" => 14} becomes [["chair", 100], ["book", 14]].
+
+
+// Convert a hash into an array of hashes using the keys from each hash as the :id key in each of the array's hashes.
+// For example, {321 => {name: "Alice", age: 31}, 322 => {name: "Maria", age: 27}} becomes [{id: 321, name: "Alice", age: 31}, {id: 322, name: "Maria", age: 27}].
+
+
+// Convert an array of strings into a hash with keys for each string in the array and values for the number of times the string appears in the array.
+// For example, ["do", "or", "do", "not"] becomes {"do" => 2, "or" => 1, "not" => 1}.
+
+
+// Convert a hash into a flat array containing all the hash’s keys and values.
+// For example, {"a" => 1, "b" => 2, "c" => 3, "d" => 4} becomes ["a", 1, "b", 2, "c", 3, "d", 4].
+
+
+// Combine data from a hash with names and prices and an array of hashes with names, colors, and weights to make a new hash.
+// For example, {"chair" => 75, "book" => 15} and [{name: "chair", color: "red", weight: 10}, {name: "book", color: "black", weight: 1}] becomes {"chair" => {price: 75, color: "red", weight: 10}, "book" => {price: 15, color: "black", weight: 1}}.
+
+
+// Convert an array of hashes into a hash of arrays, using the author as keys and the titles as values.
+// For example, [{author: "Jeff Smith", title: "Bone"}, {author: "George Orwell", title: "1984"}, {author: "Jeff Smith", title: "RASL"}] becomes {"Jeff Smith" => ["Bone", "RASL"], "George Orwell" => ["1984"]}.
+
+
+// Given a hash, create a new hash that has the keys and values switched.
+// For example, {"a" => 1, "b" => 2, "c" => 3} becomes {1 => "a", 2 => "b", 3 => "c"}.
 
